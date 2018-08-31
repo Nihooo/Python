7edd7639fc6b0391b731878247ab8415bf6c4f63
12345 <631149450@qq.com>
2018-08-31 15:39:36 +0800

    Update 4.Int.py

--- Python_Basic/4.Int.py
+++ Python_Basic/4.Int.py
@@ -1,1 +1,23 @@
+# 数字
+# python3里所有整形都是int类型
 
+# 将字符串转换为数字
+'''
+a = '123'
+print(type(a)) #查看类型
+b = int(a)
+print(type(b))
+'''
+
+# 将16进制转换为10进制
+'''
+num = 'a' # a为16进制时，就是十进制的10
+v = int(num, base=16)
+print(v)
+'''
+
+'''
+age = 5
+r = age.bit_length() #数字的二进制至少用几位来表示
+print(r)
+'''
